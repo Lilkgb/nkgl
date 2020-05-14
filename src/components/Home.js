194ec04fd9381005614138ac77815c91d4f47b26
Lilkgb <mcleanrs@comcast.net>
2020-05-13 17:04:01 -0700

    work on search function

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from './Header';
 import {store} from '../index';
 import {testFunction} from '../actions/index';
@@ -6,16 +6,39 @@ import * as firebase from 'firebase';
 import SignIn from './SignIn';
 
 function Home(props){
+  let employees = {"jwoiennwe0923n":{name: "ryan mClEan", dob: "5/31/95"}, "oiwe09vj3": {name: "mAsOod", dob: "5/4/22"}, "wjeifoaj":{name: "jeff"}, "jwoeifjabwe": {name: "sarah Do"}};
+
+  const captialize = words => words.split(' ').map( w =>  w.substring(0,1).toUpperCase()+ w.substring(1)).join(' ')
+
+  const [info, setDisplay] = useState(employees);
+  const [name, setName] = useState("");
+  
+  const handleSubmit = (evt) => {
+      evt.preventDefault();
+      alert(`Submitting Name ${name}`)
+  }
+
 
   function testRedux(){
     store.dispatch(testFunction())
   }
 
   return(
-    <div id="firebaseui-auth-container">
-      <Header />
-      <SignIn />
-      <h1>Welcome to the home component</h1>
+    <div>
+      <input
+          type="text"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+      <h1>{Object.keys(info).map((employee) => {
+        let individual = info[employee];
+        let lowercase = individual.name.toLowerCase();
+        if(lowercase.includes(name.toLowerCase())){
+        return <h1>{captialize(lowercase)}</h1>
+        } else {
+          
+        }
+      })}</h1>
       <button onClick={testRedux}>Test Reducer Function. Open console to see console log.</button>
     </div>
   )
