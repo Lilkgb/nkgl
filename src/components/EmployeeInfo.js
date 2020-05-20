01f83a0b01899899e5ac2643b78f851ae0497038
Lilkgb <mcleanrs@comcast.net>
2020-05-20 15:54:36 -0700

    add labels to employee info to style later

--- src/components/EmployeeInfo.js
+++ src/components/EmployeeInfo.js
@@ -4,8 +4,22 @@ function EmployeeInfo(props){
 
     console.log(props)
     let employee = props.currentEmployee;
+    let socialDisplay;
     
     const [status, setStatus] = useState(employee.status);
+    const [seeSocial, setSeeSocial] = useState(false);
+
+    if(seeSocial){
+        socialDisplay = <div>
+            <p>{atob(decodeURIComponent(employee.social))}</p>
+            <p onClick={() => setSeeSocial(false)} className="socialCheck">Close</p>
+        </div>
+    } else {
+        socialDisplay = <div>
+            <p>{employee.social}</p>
+            <p>To view full social please <span onClick={() => setSeeSocial(true)} className="socialCheck">Click Here</span></p>
+        </div>
+    }
 
     return (
         <div className="container">
@@ -17,7 +31,33 @@ function EmployeeInfo(props){
                     <button onClick={props.closeEmployeeInfoComponent}>Close</button>
                 </div>
                 <div>
-                    
+                    <div>
+                        <h3 className="title">Phone Number</h3>
+                        <p>{employee.phoneNumber}</p>
+                    </div>
+                    <div>
+                        <h3 className="title">Personal Email</h3>
+                        <p>{employee.personalEmail}</p>
+                    </div>
+                    <div>
+                        <h3 className="title">Work Email</h3>
+                        <p>{employee.workEmail}</p>
+                    </div>
+                    <div>
+                        <h3 className="title">Address</h3>
+                        <p>{employee.address}</p>
+                        <p>{employee.apt}</p>
+                        <p>{employee.state}</p>
+                        <p>{employee.zipCode}</p>
+                    </div>
+                    <div>
+                        <h3 className="title">Birthday</h3>
+                        <p>{employee.dob}</p>
+                    </div>
+                    <div>
+                        <h3 className="title">Social Security</h3>
+                        {socialDisplay}
+                    </div>
                 </div>
             </div>
         </div>
