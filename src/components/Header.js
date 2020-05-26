30bfe92553fed26807a9562f15ce34275a08e173
Lilkgb <mcleanrs@comcast.net>
2020-05-25 20:35:50 -0700

    add function to get window size to change header styling depending on size

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import * as firebase from 'firebase';
 import '../scss/Header.scss';
@@ -6,13 +6,46 @@ import '../scss/Header.scss';
 function Header(){
 
   const[active, setActive] = useState("activeHome");
+  const[size, setSize] = useState(null)
 
   function signOut(){
     firebase.auth().signOut();
-}
+  }
 
+  function useWindowSize() {
+    const isClient = typeof window === 'object';
+  
+    function getSize() {
+      return {
+        width: isClient ? window.innerWidth : undefined,
+        height: isClient ? window.innerHeight : undefined
+      };
+    }
+  
+    const [windowSize, setWindowSize] = useState(getSize);
+  
+    useEffect(() => {
+      if (!isClient) {
+        return false;
+      }
+      
+      function handleResize() {
+        setWindowSize(getSize());
+      }
+  
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
+    }, []);
+  
+    return windowSize;
+  }
 
-
+  const windowSize = useWindowSize();
+  if(windowSize.width >=1450){
+    console.log("big")
+  } else if(windowSize.width < 1450){
+    console.log("small")
+  }
   return(
     <div className="header">
       <NavLink exact to='/' activeClassName="active"><h3>Home</h3></NavLink>
