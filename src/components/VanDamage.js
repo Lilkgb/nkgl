c944c23173128b5e37a5344a204f1e1031fac30e
Lilkgb <mcleanrs@comcast.net>
2020-05-27 19:09:01 -0700

    add video player for any videos that are uploaded

--- src/components/VanDamage.js
+++ src/components/VanDamage.js
@@ -40,6 +40,9 @@ function VanDamage(props){
             {damageForm}
             <button className="addDamageButton" onClick={() => setAddDamageForm({state: true, info: null})}>Add Damage</button>
             <h1>Damages</h1>
+            <video width="100%" height="500" controls >
+            <source src="https://firebasestorage.googleapis.com/v0/b/nkgl-14ae0.appspot.com/o/7217dc93-8577-4199-8509-62e2c2ca9fdc%2Ftrim.658C2F71-4F62-4AFC-A9AC-3123BA4906B7.MOV?alt=media&token=dfd136e8-9a94-41c8-8034-9772e9cf4910" type="video/mp4"/>
+            </video>
             {damages}
         </div>
     )
